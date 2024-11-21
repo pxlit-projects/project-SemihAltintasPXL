4b94bd2d1f3edd32b4263985cddd756037c87f2e
SemihAltintasPXL <73435211+SemihAltintasPXL@users.noreply.github.com>
2024-11-21 17:44:12 +0100

    installed angular material and tailwind

--- frontend-web/Publishy/src/app/app.config.ts
+++ frontend-web/Publishy/src/app/app.config.ts
@@ -5,12 +5,13 @@ import { provideHttpClient, withFetch } from '@angular/common/http';
 import { provideAnimations } from '@angular/platform-browser/animations';
 
 import { routes } from './app.routes';
+import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
 
 export const appConfig: ApplicationConfig = {
   providers: [
     provideRouter(routes),
     provideClientHydration(),
     provideHttpClient(withFetch()),
-    provideAnimations()
+    provideAnimations(), provideAnimationsAsync()
   ]
 };
---
+++ frontend-web/Publishy/tailwind.config.js
@@ -0,0 +1,13 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./src/**/*.{html,js,ts,jsx,tsx}",
+    "./components/**/*.{html,js}",
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+
+}
+
